9d8432179051a9e28ccc98f99c8055b4c7e6bf1d
Kevin Gabinete <kgabinete08@gmail.com>
2017-07-12 23:13:34 -0700

    specify node version

--- controllers/imagesearchController.js
+++ controllers/imagesearchController.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Search = mongoose.model('Search');
-const {parseResults} = require('./utils/parseResults');
+const { parseResults } = require('./utils/parseResults');
 const axios = require('axios');
 
 exports.homePage = (req, res) => {
-  res.render('index', {title: 'Imagesearch Abstraction'});
+  res.render('index', { title: 'Imagesearch Abstraction' });
 };
 
 exports.search = async (req, res) => {
@@ -13,17 +13,20 @@ exports.search = async (req, res) => {
   if (!req.query.offset || req.query.offset == 0) {
     offset = 1;
   }
-  await new Search({term: searchTerm}).save();
-  // prettier-ignore
-  const searchRequest = await axios.get(`https://www.googleapis.com/customsearch/v1?q=${searchTerm}&cx=${process.env.CSE_ID}&safe=medium&searchType=image&start=${offset}&fields=items(image(contextLink%2CthumbnailLink)%2Clink%2Csnippet)%2Curl&key=${process.env.SEARCH_KEY}`);
+  await new Search({ term: searchTerm }).save();
+  const searchRequest = await axios.get(
+    `https://www.googleapis.com/customsearch/v1?q=${searchTerm}&cx=${process.env
+      .CSE_ID}&safe=medium&searchType=image&start=${offset}&fields=items(image(contextLink%2CthumbnailLink)%2Clink%2Csnippet)%2Curl&key=${process
+      .env.SEARCH_KEY}`
+  );
   const resultData = searchRequest.data.items;
   const results = parseResults(resultData);
   res.json(results);
 };
 
 exports.history = async (req, res) => {
-  const list = await Search.find({}, {_id: 0, __v: 0})
+  const list = await Search.find({}, { _id: 0, __v: 0 })
     .limit(10)
-    .sort({when: -1});
+    .sort({ when: -1 });
   res.json(list);
 };
